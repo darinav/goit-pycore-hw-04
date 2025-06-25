f57a49313a228f6cc162c2d6cf1f80af4faed1b3
Daryna Voloshyna <voloshyna.darina@gmail.com>
2025-06-25 20:13:20 +0300

    Improve error handling in task 4

--- Task4/task_4.py
+++ Task4/task_4.py
@@ -1,13 +1,20 @@
 def parse_input(user_input):
-    cmd, *args = user_input.strip().split()
+    parts = user_input.strip().split()
+    if not parts:
+        return None
+    cmd, *args = parts
     return cmd.lower(), args
 
 def add_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid input format."
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
 def change_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid input format."
     name, phone = args
     if name in contacts:
         contacts[name] = phone
@@ -22,38 +29,38 @@ def show_phone(args, contacts):
     else:
         return "Contact not found."
 
-
 def show_all(contacts):
     if not contacts:
         return "No contacts saved."
     return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
-
 
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
 
     while True:
-        user_input = input("Enter a command: ")
-        command, args = parse_input(user_input)
+        if (user_input := input("Enter a command: ")) in ("exit", "close"):
+            print("Good bye!")
+            break
 
-        match command:
-            case "exit" | "close":
-                print("Good bye!")
-                break
-            case "hello":
-                print("How can I help you?")
-            case "add":
-                print(add_contact(args, contacts))
-            case "change":
-                print(change_contact(args, contacts))
-            case "phone":
-                print(show_phone(args, contacts))
-            case "all":
-                print(show_all(contacts))
-            case _:
-                print("Invalid command.")
+        if parsed := parse_input(user_input):
+            command, args = parsed
 
+            match command:
+                case "hello":
+                    print("How can I help you?")
+                case "add":
+                    print(add_contact(args, contacts))
+                case "change":
+                    print(change_contact(args, contacts))
+                case "phone":
+                    print(show_phone(args, contacts))
+                case "all":
+                    print(show_all(contacts))
+                case _:
+                    print("Invalid command.")
+        else:
+            print("Invalid input format.")
 
 if __name__ == "__main__":
     main()
